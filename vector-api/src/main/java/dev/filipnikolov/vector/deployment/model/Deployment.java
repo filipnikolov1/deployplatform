d0ffa454164b76223d3413474fbdf16a2563acc8
Filip <filip4mk@gmail.com>
2026-04-25 23:46:44 +0200

    add missing events imports to JPA entities

--- vector-api/src/main/java/dev/filipnikolov/vector/deployment/model/Deployment.java
+++ vector-api/src/main/java/dev/filipnikolov/vector/deployment/model/Deployment.java
@@ -1,6 +1,7 @@
 package dev.filipnikolov.vector.deployment.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.filipnikolov.vector.events.DeploymentStatus;
 import jakarta.persistence.*;
 import lombok.Data;
 
