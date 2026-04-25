46dd940bee12a3fbb41f1e1718bf95c66e3de27e
Filip <filip4mk@gmail.com>
2026-04-26 00:20:26 +0200

    add events sse endpoint

---
+++ vector-api/src/main/java/dev/filipnikolov/vector/sse/EventStreamController.java
@@ -0,0 +1,21 @@
+package dev.filipnikolov.vector.sse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequestMapping("/api/events")
+@RequiredArgsConstructor
+public class EventStreamController {
+
+    private final PostgresListenService listenService;
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter stream() {
+        return listenService.register();
+    }
+}
