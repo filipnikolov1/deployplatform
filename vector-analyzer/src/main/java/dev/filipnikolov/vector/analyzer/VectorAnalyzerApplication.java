7489d92ca7836c8518f560d91561b6d3bbcc912c
Filip <filip4mk@gmail.com>
2026-04-30 18:00:45 +0200

    scaffold vector-analyzer spring boot project

---
+++ vector-analyzer/src/main/java/dev/filipnikolov/vector/analyzer/VectorAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package dev.filipnikolov.vector.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VectorAnalyzerApplication.class, args);
+    }
+}
